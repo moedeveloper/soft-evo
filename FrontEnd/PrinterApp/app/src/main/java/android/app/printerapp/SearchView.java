0d0e255b4b1534c6d704dceebd967398f362ac2f
Alex Tao <talex@student.chalmers.se>
2017-11-23 15:56:00 +0100

    Rebuilt the search layout

---
+++ FrontEnd/PrinterApp/app/src/main/java/android/app/printerapp/SearchView.java
@@ -0,0 +1,14 @@
+package android.app.printerapp;
+
+import android.content.Context;
+import android.widget.LinearLayout;
+
+/**
+ * Created by SAMSUNG on 2017-11-23.
+ */
+
+public class SearchView extends LinearLayout {
+    public SearchView(Context context) {
+        super(context);
+    }
+}
